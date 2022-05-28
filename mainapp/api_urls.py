6af82d20d184b286c767d7692a0b1acb4b11c7a4
1996p <dursinear@gmail.com>
2022-05-28 21:29:29 +0700

    added put method; able to create product with serializer's method .create() able to update product with serializer's method .update()

--- mainapp/api_urls.py
+++ mainapp/api_urls.py
@@ -3,5 +3,6 @@ from .api_views import *
 
 urlpatterns = [
     path('v1/productlist/', ProductAPIView.as_view()),
+    path('v1/product/<int:pk>/', ProductAPIView.as_view()),
 
 ]
--- mainapp/api_views.py
+++ mainapp/api_views.py
@@ -15,18 +15,24 @@ class ProductAPIView(APIView):
     def post(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        new_data = ProductSerializer(request.data).data
-        product = Product.objects.create(
-            title=new_data['title'],
-            description=new_data['description'],
-            actual_price=new_data['actual_price'],
-            old_price=new_data['old_price'],
-            sale_status=new_data['sale_status'],
-            author=SiteUser.objects.get(user__username=request.data['author']),
-            image=new_data['image'],
-            category=Category.objects.get(name=request.data['category']),
-            published=new_data['published']
-        )
+        serializer.save()
 
-        product.save()
-        return Response(ProductSerializer(product).data)
+        return Response(serializer.data)
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+
+        if not pk:
+            return Response({'error': 'Method is not allowed'})
+
+        try:
+            product = Product.objects.get(pk=pk)
+        except Exception:
+            return Response({'error': 'Product with same PK doesn\'t exist'})
+
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({'product': serializer.data})
+
